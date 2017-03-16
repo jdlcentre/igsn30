e5b3d809b846901465e534eb023bde8db4505bc8
Victor Tey <victor.tey@csiro.au>
2017-03-16 15:55:53 +0800

    Remap comments as descriptions

--- src/main/java/org/csiro/igsn/jaxb/oai/bindings/igsn/EntityToSchemaConverterIGSN.java
+++ src/main/java/org/csiro/igsn/jaxb/oai/bindings/igsn/EntityToSchemaConverterIGSN.java
@@ -277,7 +277,7 @@ public class EntityToSchemaConverterIGSN implements JAXBConverterInterface{
 		}
 		
 		//VT:Comments and description are not exactly the same thing.
-		//resourceXML.setDescription(resource.getComments());
+		resourceXML.setDescription(resource.getComments());
 		
 		resourceXML.setRegistrant(this.objectFactory.createResourceRegistrant());
 		resourceXML.getRegistrant().setName(this.REGISTRANT_NAME);
